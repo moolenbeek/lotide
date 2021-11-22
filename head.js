431790d6535f530ac5ade04c21acf02d28218095
Eric Moolenbeek <emoolenbeek@gmail.com>
2021-11-22 20:51:50 +0000

    commented out test code

--- head.js
+++ head.js
@@ -12,8 +12,8 @@ const head = (data) => {
 }
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
