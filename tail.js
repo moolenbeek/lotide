328b2ca2f4f9b94970457a674ed431f67418bfbd
Eric Moolenbeek <emoolenbeek@gmail.com>
2021-11-22 20:52:18 +0000

    commented out test code

--- tail.js
+++ tail.js
@@ -16,20 +16,20 @@ const tail = (data) => {
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
 
 // CANNOT COMPARE ARRAYS WITH THIS METHOD, WONT PASS ASSERTION
 // assertEqual(tail([5,6,7]), [6,7]);
 // assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ['Lighthouse','Labs']);
 
 // Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
 // Test Case 2: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
