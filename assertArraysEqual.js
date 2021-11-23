916be15925faa12981d7c4c37b1f1d6b6c1295f4
Eric Moolenbeek <emoolenbeek@gmail.com>
2021-11-23 20:23:02 +0000

    changed function name

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const eqArrays = (arr1, arr2) => {
+const eqArraysassertArraysEqual = (arr1, arr2) => {
   if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
     console.log(`true: ${arr1} === ${arr2}`)
   } else {
@@ -6,8 +6,8 @@ const eqArrays = (arr1, arr2) => {
   }
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
