3458535f1a25347c985a393227b6c7686105b3cb
Eric Moolenbeek <emoolenbeek@gmail.com>
2021-11-23 20:54:18 +0000

    lint assertArraysEqual. write without func

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,13 @@
 const assertArraysEqual = (arr1, arr2) => {
   if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
-    console.log(`true: ${arr1} === ${arr2}`)
+    console.log(`true: ${arr1} === ${arr2}`);
   } else {
-    console.log(`false: ${arr1} != ${arr2}`)
+    console.log(`false: ${arr1} != ${arr2}`);
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
---
+++ without.js
@@ -0,0 +1,51 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+//////////////////////////////////////////////////////////////////////////
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
+    console.log(`true: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`false: ${arr1} != ${arr2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+
+//////////////////////////////////////////////////////////////////////////
+
+const without = (source, itemsToRemove) => {
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        source.splice(i, 1);
+      }
+    }
+  }
+  let newArr = source;
+  return newArr;
+};
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
