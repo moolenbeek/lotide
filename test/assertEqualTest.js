1a23acf670afcc84d7ae0437742d8f203e3c1c51
Eric Moolenbeek <emoolenbeek@gmail.com>
2021-11-29 19:05:59 +0000

    add test & link files for tail

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,5 +1,6 @@
 const assertEqual = require('../assertEqual');
 const head = require('../head');
+const tail = require('../tail');
 
 // assertEqual test
 console.log('\n test asssert equal');
@@ -10,3 +11,14 @@ assertEqual(1, 1);
 console.log('\n test head');
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+// tail test
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// Test Case 2: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
